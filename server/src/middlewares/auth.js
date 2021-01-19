39d19f690841a413143e2865b0c3d857f787212c
shahjalal <lx.shahjalal@gmail.com>
2021-01-19 22:06:51 +0600

    jwt authentication completed

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  jwt.verify('jwt', process.env.SECRET || 'test', (err, token) => {
+    // if (err) throw new Error('You are not allowed to browse this route');
+    console.log(token);
+  });
+  next();
+};
